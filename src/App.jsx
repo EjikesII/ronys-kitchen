ad1f1393633242e9aa7f12bd5db7cf0f962113ac
EjikeII <ejix22@gmail.com>
2023-09-15 13:46:57 +0100

    worked on the menu items

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import items from "./data";
 import Title from "./Title";
 import Menu from "./menu";
+import menu from "./data";
+import Categories from "./Categories";
 
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+  
   return(
       <main>
         <section className="menu">
-          <Title text = "Rony's Kitchen" />
+          <Title text = " Rony's Kitchen" />
+          <Categories categories={categories} />
           <Menu items={menuItems} />
+          
         </section>
      </main>
   )
--- src/Title.jsx
+++ src/Title.jsx
@@ -3,7 +3,7 @@ import App from "./App"
 function Title({text}) {
     return (
         <div className="title">
-            <h2> {text || 'Ronys Kitchen'}</h2>
+            <h2> {text || 'Max Kitchen'}</h2>
         <div className="title-underline"> </div>
         </div>
     )
